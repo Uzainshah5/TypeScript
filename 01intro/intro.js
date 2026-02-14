ce946ea5eb6c1a6db9ec61d18759693169c562e8
UzainAhmadShah <Uzainshah22@gmail.com>
2026-02-14 12:40:35 +0530

    Codded few lines

---
+++ 01intro/intro.js
@@ -0,0 +1,4 @@
+var user = { name: "Saifullah", age: 10 };
+console.log("user.name");
+console.log("Uzain");
+console.log(user.email);
---
+++ 01intro/intro.ts
@@ -0,0 +1,5 @@
+let user = {name: "Saifullah", age :10}
+console.log("user.name");
+
+console.log("Uzain");
+console.log(user.email)
