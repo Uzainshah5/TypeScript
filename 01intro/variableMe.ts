40893b43771bcdc50012fcf70ac22c2bbd869246
UzainAhmadShah <Uzainshah22@gmail.com>
2026-02-14 14:48:11 +0530

    Varaiables type

--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -1,5 +1,7 @@
 let greetings : string = "Hello Uzain"
-let mynum = 6 ;
+//Number 
+//let mynum : number = 6 ; //bad
+let mynum = 6 //good type inference 
 greetings.toLowerCase()
 console.log(greetings);
 
